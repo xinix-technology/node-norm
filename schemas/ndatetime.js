ce394d4cf2d1bb3dfe0f47a662c4f5c8158d3fae
Ganesha <reekoheek@gmail.com>
2018-10-08 14:12:59 +0700

    NDatetime attach from number

--- schemas/ndatetime.js
+++ schemas/ndatetime.js
@@ -2,6 +2,20 @@ const NField = require('./nfield');
 
 module.exports = class NDatetime extends NField {
   attach (value) {
+    if (!value) {
+      return;
+    }
+
+    if (typeof value === 'string' && !isNaN(value)) {
+      value = Number(value);
+    }
+
+    if (typeof value === 'number') {
+      let date = new Date();
+      date.setTime(value);
+      return date;
+    }
+
     let date = new Date(value);
     if (!isNaN(date.getTime())) {
       return date;
